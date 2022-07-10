a22d3ba56d6a4f4798220d022aeef5ac8cd1f15b
Dilmi <h.dilmi1999@gmail.com>
2022-07-10 18:31:42 +0700

    TUTE_2-Scala

--- q4_b.scala
+++ q4_b.scala
@@ -1,7 +1,7 @@
 object q4_b extends App {
    def attendees(price:Int):Int=120+(15-price)/5*20;
    def revenue(price:Int):Int = attendees(price)*price;
-   def cost(price:Int):Int=500+attendees(price);
+   def cost(price:Int):Int=500+attendees(price)*3;
    def profit(price:Int):Int = revenue(price)- cost(price);
    print(profit(10),profit(15),profit(20));
 
